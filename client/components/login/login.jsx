27e3eba98e74c6114208a32fff9545a41f9086e4
Luis Rojas <lrojas94@gmail.com>
2016-05-21 13:39:06 -0400

    Added custom SASS file * Added PUCMM bg class * Modified button default to be a PUCMM like color.

--- client/components/login/login.jsx
+++ client/components/login/login.jsx
@@ -7,7 +7,7 @@ export var Login = React.createClass({
           <div className="modal fade" tabindex="-1" role="dialog" id='loginModal'>
             <div className="modal-dialog">
               <div className="modal-content">
-                <div className="modal-header">
+                <div className="modal-header pucmm-bg">
                   <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                   <h4 className="modal-title">Autenticacion</h4>
                 </div>
@@ -23,7 +23,7 @@ export var Login = React.createClass({
                     </div>
                   </div>
                   <div className="modal-footer">
-                    <input type="submit" className='btn btn-success btn-block' value='Iniciar Sesion'/>
+                    <input type="submit" className='btn btn-default btn-block' value='Iniciar Sesion'/>
                   </div>
 
                 </form>
