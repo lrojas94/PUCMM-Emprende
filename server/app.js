6ca949c5be5fdafdde854c701dd82ea7b7553098
Edward Romero Espinal <edwardespinal16@gmail.com>
2016-05-21 12:29:53 -0400

    placeholder

--- server/app.js
+++ server/app.js
@@ -4,6 +4,24 @@ var bodyParser = require('body-parser');
 var app = express();
 var idea_routings = require('./controllers/idea');
 
+var passport = require('passport');
+
+// This is the file we created in step 2.
+// This will configure Passport to use Auth0
+var strategy = require('./setup-passport');
+
+// Session and cookies middlewares to keep user logged in
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+app.use(cookieParser());
+// See express session docs for information on the options: https://github.com/expressjs/session
+app.use(session({ secret: 'Mw1DzDOYvZqchGSxVHI5-3F2Y0ITQW4fzUHg18GCIoecEG0Hw6RhgHEr4mNknAHe', resave: false,  saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.use(express.static('client/'));
 app.use(express.static('bower_components/'));
@@ -21,6 +39,17 @@ app.post('/api',function(req,res){
 	console.log(req.body); //<- This is for a Post
 	res.send({"data" : "Working"});
 });
+
+
+app.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.redirect("localhost:3000")
+		console.log('asdfasdfadsfasdf');
+  });
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname + './../client/index.html'));
