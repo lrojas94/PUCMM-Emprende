fe5887c32378f2f101897832dd67930f8216f22e
Luis Rojas <lrojas94@gmail.com>
2016-05-22 13:21:40 -0400

    Fixed footer

--- client/components/footer/footer.jsx
+++ client/components/footer/footer.jsx
@@ -4,16 +4,15 @@ import {Link} from 'react-router';
 export var Footer = React.createClass({
   render: function() {
     return (
-			<div className="container-fluid">
-        <div className="footer-main">
+			  <div className="footer-main">
           <div className="container">
-            <div class="row">
+            <div className="row">
 
-               <div className="col-sm-4 col-md-4">
+               <div className="col-sm-4 col-xs-12 col-md-4">
                <br/>
                 <div className="caption">
                 <h4>Campus Santiago</h4>
-                  <div className="separator-div-footer"/>
+                  <div className="separator-div-footer"></div>
                     <p>Autopista Duarte Km 1 1/2<br/>
                       Santiago, Republica Dominicana.<br/>
                       Tel: (809) 580-1962<br/>
@@ -22,11 +21,11 @@ export var Footer = React.createClass({
                 </div>
                </div>
 
-               <div className="col-sm-4 col-md-4">
+               <div className="col-sm-4 col-xs-12 col-md-4">
                <br/>
                 <div className="caption">
                 <h4>Campus Santo Tomas de Aquino</h4>
-                  <div className="separator-div-footer"/>
+                  <div className="separator-div-footer"></div>
                     <p>
                       Abraham Lincoln esq. Rómulo Betancourt<br/>
                       Santo Domingo, Republica Dominicana.<br/>
@@ -36,11 +35,11 @@ export var Footer = React.createClass({
                 </div>
                </div>
 
-               <div className="col-sm-4 col-md-4">
+               <div className="col-sm-4 col-xs-12 col-md-4">
                <br/>
                 <div className="caption">
                 <h4>Extension Puerto Plata</h4>
-                  <div className="separator-div-footer"/>
+                  <div className="separator-div-footer"></div>
                     <p>Calle Separacion No. 2<br/>
                       Puerto Plata, Republica Dominicana.<br/>
                       Tel: (809) 586-2060<br/>
@@ -55,7 +54,6 @@ export var Footer = React.createClass({
             <p style={{textAlign:"center"}}>© Copyright 2016 Hackaton PUCMM Emprende</p>
           </div>
         </div>
-			</div>
     );
   }
 });
