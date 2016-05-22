fb0317819369c336457457d1fd9247f653500c3d
Luis Rojas <lrojas94@gmail.com>
2016-05-22 07:01:42 -0400

    Bowerito done o/

--- client/components/projects/projects.jsx
+++ client/components/projects/projects.jsx
@@ -4,21 +4,62 @@ import {Link} from 'react-router';
 export var Projects = React.createClass({
   render: function() {
     return (
-			<div className="container">
+			<div className="container idea-container">
       {/*-------------------------This will load tables Dinamically*/}
       {/*-------------------------This is the IDEA_INFO part*/}
-        <div className="row flex-container">
-              <div className="col-xs-3 col-md-2 flex-item">
-                <img className="project-thumbnail center-block" src="http://img14.deviantart.net/0da8/i/2014/035/a/6/random_monster_by_ronyeryx-d753zl3.png"/>
+        <div className="row flex-container project-divisor">
+              <div className="col-xs-3 img-idea-parent col-md-2 flex-item">
+                <img className="img-idea"
+                  src="http://bower.io/img/bower-logo.png"/>
               </div>
-              <div className="col-xs-9 col-md-10 flex-item">
-                <div style={{backgroundColor:"#1066a4", color:"#FFF", margin:"0"}}>
-                  IDEA_NAME
+              <div className="col-xs-9 col-md-10 flex-item flex-col">
+                <div className='row pucmm-bg'>
+                  <div className='col-xs-12'>
+                    <h2>Bowerito</h2>
+                  </div>
                 </div>
-                <div style={{backgroundColor:"#e0f2f2", margin:"0"}}>
-                  IDEA_PROBLEM_SOLVED
+                <div className='row' style={{backgroundColor:"#e0f2f2"}}>
+                  <div className='col-xs-12'>
+                    <h4>Empresa de Desarrollo de Software</h4>
+                  </div>
                 </div>
               </div>
+        </div>
+        <div className='row'>
+          <div className='col-xs-12'>
+            <h4>Mentor en Jefe:<span className='pull-right'>Miguel German<small className='text-primary'> (Mercadologo)</small></span></h4>
+          </div>
+          <div className='col-xs-12'>
+            <h4>Mentor(es):<span className='pull-right'>Juan Miguel</span></h4>
+          </div>
+          <div className='col-xs-12'>
+            <h4>Notas Personales</h4>
+            <ul className='list-group'>
+              <li className='list-group-item'>
+                Leer libro sobre Mercados <span className="label label-danger pull-right">Urgente</span>
+              </li>
+              <li className='list-group-item'>
+                Enviar propuesta a BanReservas
+              </li>
+            </ul>
+          </div>
+          <div className='col-xs-12'>
+            <h4>Etapas del Proyecto</h4>
+            <div className='col-xs-3 text-center'><h5>Aprovacion</h5></div>
+            <div className='col-xs-3 text-center'><h5>Refuerzo</h5></div>
+            <div className='col-xs-3 text-center'><h5>Evaluacion</h5></div>
+            <div className='col-xs-3 text-center'><h5>Ejecucion</h5></div>
+            <div className='col-xs-12'>
+              <div className="progress">
+                <div className="progress-bar progress-bar-primary"
+                  role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"
+                  style={{width: '25%'}}>
+                  <span className="sr-only">25% Complete (success)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       {/*----------------------------------------------------------*/}
       </div>
